5bcd91695dcaffc221d2cee3c533a03fdc642c15
Scott Davis <sc.ttdav.s@gmail.com>
2016-04-02 17:30:14 -0400

    refactor

--- klassy.js
+++ klassy.js
@@ -8,35 +8,45 @@
     return key.indexOf('$') === 0;
   };
 
-  var saveStatic = function(obj, newKlass) {
-    var keys = Object.keys(obj); // save keys so we can delete them from obj without affecting looping/indexes
-    for (var i = 0; i < keys.length; i++) {
-      var key = keys[i];
-
-      if (isStaticKey(key) && obj.hasOwnProperty(key)) {
-        var newKey = key.slice(1); // remove $
-        newKlass[newKey] = obj[key]; // save static method on klass
-        delete obj[key]; // delete off prototype
+  var extend = function(init, parent) {
+    var proto = {};
+    
+    for (var key in parent) {
+      if (parent.hasOwnProperty(key)) {
+        // direct property
+        if (isStaticKey(key)) {
+          var newKey = key.slice(1); // remove $
+          init[newKey] = parent[key]; // save static method on klass
+        } else {
+          // instance method, save on prototype
+          proto[key] = parent[key];
+        }
+      } else {
+        // found on prototype
+        proto[key] = parent[key]
       }
     }
+
+    init.prototype = proto;
+
+    return init;
   };
 
-  var klass = function(obj) {
-    obj = obj || {}
-    var constructor = obj.init;
+  var klass = function(options) {
+    options = options || {}
+    var init = options.init;
 
-    if (constructor) {
-      // delete init since obj will be the prototype
-      delete obj.init;
+    if (init) {
+      // delete init since options will be the prototype
+      delete options.init;
     }  else {
       // no init, so use a default
-      constructor = function() {};
+      init = function() {};
     }
     // saves and removes static methods denoted with $
-    saveStatic(obj, constructor);
-    constructor.prototype = obj;
+    init = extend(init, options);
 
-    return constructor;
+    return init;
   };
 
   return klass;

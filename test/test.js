7cf2109c3da0d1b081a4deb414a8745f50bb1c70
Scott Davis <sc.ttdav.s@gmail.com>
2016-04-02 18:54:03 -0400

    fix test

--- test/test.js
+++ test/test.js
@@ -6,62 +6,63 @@ var klassy = require("../klassy");
 
 describe("Klassy", function() {
 
-  // it("should create a default", function() {
-  //   var Person = klassy();
-  //   var bill = new Person();
+  it("should create a default", function() {
+    var Person = klassy();
+    var bill = new Person();
 
-  //   expect(Person).to.be.a("function");
-  //   expect(bill).to.be.an("object");
-  // });
+    expect(Person).to.be.a("function");
+    expect(bill).to.be.an("object");
+  });
 
-  // it("should create a klass", function() {
-  //   var Person = klassy({
-  //     init: function(name) {
-  //       this.name = name;
-  //     },
-  //     say: function() {
-  //       return this.name;
-  //     }
-  //   });
+  it("should create a klass", function() {
+    var Person = klassy({
+      init: function(name) {
+        this.name = name;
+      },
+      say: function() {
+        return this.name;
+      }
+    });
 
-  //   var bill = new Person("Bill");
-  //   expect(bill.say()).to.equal("Bill");
-  // });
+    var bill = new Person("Bill");
+    expect(bill.say()).to.equal("Bill");
+  });
 
-  // it("should save a static method", function() {
-  //   var Person = klassy({
-  //     init: function(name) {
-  //       this.name = name;
-  //     },
-  //     $say: function() {
-  //       return "static";
-  //     }
-  //   });
+  it("should save a static method", function() {
+    var Person = klassy({
+      init: function(name) {
+        this.name = name;
+      },
+      $say: function() {
+        return "static";
+      }
+    });
 
-  //   var bill = new Person("Bill");
-  //   expect(bill.say).to.be.undefined;
-  //   expect(Person.say()).to.equal("static")
-  // });
+    var bill = new Person("Bill");
+    expect(bill.say).to.be.undefined;
+    expect(Person.say()).to.equal("static")
+  });
 
-  // it("should extend another klass", function() {
-  //   var Animal = klassy({
-  //     init: function(name) {
-  //       this.name = name;
-  //     },
-  //     $say: function() {
-  //       return "animal";
-  //     }
-  //   });
+  it("should extend another klass", function() {
+    var Animal = klassy({
+      init: function(name) {
+        this.name = name;
+      },
+      $say: function() {
+        return "animal";
+      }
+    });
 
-  //   var Dog = Animal.extend();
-  //   var lassie = new Dog("Lassie");
-  //   var cat = new Animal("Garfield");
+    var Dog = Animal.extend();
+    var lassie = new Dog("Lassie");
+    var cat = new Animal("Garfield");
 
-  //   expect(cat.name).to.equal("Garfield");
-  //   expect(Animal.say()).to.equal("animal");
-  //   expect(lassie.say()).to.equal("animal");
-  //   expect(lassie.name).to.equal("Lassie");
-  // });
+    expect(cat.name).to.equal("Garfield");
+    expect(Animal.say()).to.equal("animal");
+    expect(Dog.say()).to.equal("animal");
+    expect(lassie.say).to.be.undefined;
+    expect(lassie.name).to.equal("Lassie");
+  });
 
   it("should extend another klass with overrides/new methods", function() {
     var Animal = klassy({
